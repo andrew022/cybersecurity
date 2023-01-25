2ca9922e13f8b4781479b165290de358ada1c34c
Andrew <asiterlet.as@outlook.com>
2023-01-24 20:03:36 -0600

    Challenges

--- 301_Challenges/301challenge02.sh
+++ 301_Challenges/301challenge02.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-cp /var/log/syslog /home/andrew/repos1/301-02/syslog.$current_time
+cp /var/log/syslog /home/andrew/cybersecurity/301_Challenges/301-02/syslog.$current_time
